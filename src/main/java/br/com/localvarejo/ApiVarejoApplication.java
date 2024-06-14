aa46cb94f187d0f05985675979c791df1eef8d23
Vitor Bonato <vitorbonatocam@gmail.com>
2024-06-13 21:41:33 -0300

    First commit

---
+++ src/main/java/br/com/localvarejo/ApiVarejoApplication.java
@@ -0,0 +1,13 @@
+package br.com.localvarejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVarejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVarejoApplication.class, args);
+	}
+
+}
